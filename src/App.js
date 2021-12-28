12208080a5044ce0f54cbdec469f2caa5306083b
Darito <davidalonso991@gmail.com>
2021-12-27 18:56:43 -0600

    Convertir el componente app en clase y agregar estado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    this.state = {
+      renderedComponent: "getInformation",
+    };
+    this.ChangeRenderedComponent = this.ChangeRenderedComponent.bind(this);
+  }
+  render() {
+    return (
+      <div className="App">
+        {renderedComponent === "getInformation" ? "" : ""}
+      </div>
+    );
+  }
 }
 
 export default App;
