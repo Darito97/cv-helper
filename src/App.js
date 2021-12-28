726342a9af23a9f14eb0be400d96aeacab94a3e4
Darito <davidalonso991@gmail.com>
2021-12-27 19:32:09 -0600

    creado de componente de clase Forms

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import React from "react";
 
+import Forms from './components/Forms'
+
 class App extends React.Component {
   constructor() {
+    super()
     this.state = {
       renderedComponent: "getInformation",
     };
     this.ChangeRenderedComponent = this.ChangeRenderedComponent.bind(this);
   }
+  ChangeRenderedComponent(componentToRender){
+
+  }
   render() {
     return (
       <div className="App">
-        {renderedComponent === "getInformation" ? "" : ""}
+        {this.state.renderedComponent === "getInformation" ? <Forms /> : "Mostrar info"}
       </div>
     );
   }
---
+++ src/components/Forms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Forms extends React.Component{
+  
+  render(){
+    const miaw = 'miaw'
+    return miaw
+  }
+}
+
+export default Forms
