7196ae80233056e7289a773bcaced9d529448a46
Darito <davidalonso991@gmail.com>
2021-12-31 06:53:57 -0600

    Updates

---
+++ precache-manifest.63dcfdb842394cac4c861038dc281c59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4829273c3070a3dba134",
+    "url": "/cv-helper/static/css/main.76cf3177.chunk.css"
+  },
+  {
+    "revision": "4829273c3070a3dba134",
+    "url": "/cv-helper/static/js/main.4829273c.chunk.js"
+  },
+  {
+    "revision": "2057a59fa3938e60c5cc",
+    "url": "/cv-helper/static/js/1.2057a59f.chunk.js"
+  },
+  {
+    "revision": "c86127f0cc3910f83ba8",
+    "url": "/cv-helper/static/js/runtime~main.c86127f0.js"
+  },
+  {
+    "revision": "8a8d094b20e95b6a3464c6fcb9585734",
+    "url": "/cv-helper/index.html"
+  }
+];
